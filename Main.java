a944dea2ef1db1c24eda7a7880e4532ef041b9bb
Durgesh mahajan <153438873+DDurgeshmahajan@users.noreply.github.com>
2025-10-23 15:08:53 +0530

    Code

---
+++ Main.java
@@ -0,0 +1,14 @@
+/*
+ * Author: Durgesh Mahajan
+ * Date: 2023-10-01
+ * Project: Lost Explorer's Tale
+ */
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        GameManager gameManager = new GameManager();
+        gameManager.runGame();
+    }
+}
